6e975e3a648199cf9a3ac8665239c380c60bb402
kenkauluwehi <ken@indiereign.com>
2014-12-09 20:51:30 -1000

    fixing push

--- config/initializers/setup-mail.rb
+++ config/initializers/setup-mail.rb
@@ -1,7 +1,7 @@
-config.action_mailer.default_url_options = {host: 'www.heroku.com'}
-config.action_mailer.delivery_method = :stmp
-config.action_mailer.stmp_settings = {
-# ActionMailer::Base.smtp_settings = {
+# config.action_mailer.default_url_options = {host: 'www.heroku.com'}
+# config.action_mailer.delivery_method = :stmp
+# config.action_mailer.stmp_settings = {
+ActionMailer::Base.smtp_settings = {
 	:address					=> "smtp.sendgrid.net",
 	:port						=> 587,
 	:domain						=> "heroku.com",
